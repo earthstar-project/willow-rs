31da9a36a958d66b35bfea4af0ccc08e49d2631a
Sam Gwilym <sam@gwil.garden>
2024-07-09 11:36:52 +0100

    Use TestConsumer in path fuzz test

--- fuzz/fuzz_targets/path_encoding.rs
+++ fuzz/fuzz_targets/path_encoding.rs
@@ -1,7 +1,7 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use ufotofu::local_nb::{consumer::IntoVec, producer::FromVec};
+use ufotofu::local_nb::{consumer::TestConsumer, producer::FromVec, BufferedConsumer};
 use willow_data_model::{
     encoding::path::{decode_path, encode_path},
     path::PathRc,
@@ -11,18 +11,25 @@ const MCL: usize = 111111;
 const MCC: usize = 111111;
 const MPL: usize = 111111;
 
-fuzz_target!(|path: PathRc<MCL, MCC, MPL>| {
-    // Use a TestConsumer instead of IntoVec
-    let mut consumer = IntoVec::<u8>::new();
+fuzz_target!(|data: (PathRc<MCL, MCC, MPL>, TestConsumer<u8, u8, u8>)| {
+    let (path, mut consumer) = data;
 
     smol::block_on(async {
-        // I want to test that the correct error is returned given the how BAD our testconsumer is
-        encode_path::<MCL, MCC, _, _>(&path, &mut consumer)
-            .await
-            .unwrap();
+        let consumer_should_error = consumer.should_error();
 
-        // THis should eventually be a testproducer
-        let mut producer = FromVec::new(consumer.into_vec());
+        if let Err(_err) = encode_path::<MCL, MCC, _, _>(&path, &mut consumer).await {
+            assert!(consumer_should_error);
+            return;
+        }
+
+        consumer.flush().await.unwrap();
+
+        let mut new_vec = Vec::new();
+
+        new_vec.extend_from_slice(consumer.as_ref());
+
+        // THis should eventually be a testproducer, when we can initialise one of those with some known data.
+        let mut producer = FromVec::new(new_vec);
 
         // Check for correct errors
         let decoded_path = decode_path::<MCL, MCC, _, PathRc<MCL, MCC, MPL>>(&mut producer)
