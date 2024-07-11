755438058613cc8be9955c0103f73e91d2e38c09
Sam Gwilym <sam@gwil.garden>
2024-07-11 11:58:03 +0100

    Improve Encoder / Decoder docs

--- data-model/src/encoding/parameters.rs
+++ data-model/src/encoding/parameters.rs
@@ -3,8 +3,13 @@ use std::future::Future;
 use crate::encoding::error::{DecodeError, EncodingConsumerError};
 use ufotofu::local_nb::{BulkConsumer, BulkProducer};
 
+/// A type that can be encoded to a bytestring, ensuring that any value of `Self` maps to exactly one bytestring.
+///
+/// [Definition](https://willowprotocol.org/specs/encodings/index.html#encodings_what)
 pub trait Encoder {
-    /// A function from the set `Self` to the set of bytestrings, ensuring that any value of `Self` maps to exactly one bytestring.
+    /// A function from the set `Self` to the set of bytestrings.
+    ///
+    /// [Definition](https://willowprotocol.org/specs/encodings/index.html#encode_s)
     fn encode<Consumer>(
         &self,
         consumer: &mut Consumer,
@@ -13,8 +18,13 @@ pub trait Encoder {
         Consumer: BulkConsumer<Item = u8>;
 }
 
+/// A type that can be decoded from a bytestring, ensuring that every valid encoding maps to exactly one member of `Self`.
+///
+/// [Definition](https://willowprotocol.org/specs/encodings/index.html#encodings_what)
 pub trait Decoder {
-    /// A function from the set of bytestrings to the set of `T`, ensuring that every valid encoding maps to exactly one member of `T`.
+    /// A function from the set of bytestrings to the set of `T`.
+    ///
+    /// [Definition](https://willowprotocol.org/specs/encodings/index.html#decode_s)
     fn decode<Producer>(
         producer: &mut Producer,
     ) -> impl Future<Output = Result<Self, DecodeError<Producer::Error>>>
