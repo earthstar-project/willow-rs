57ac3139c98e34229062efb3125988a90bbde35b
Sam Gwilym <sam@gwil.garden>
2024-07-11 12:21:31 +0100

    Refactor CompactWidth a bit

--- data-model/src/encoding/compact_width.rs
+++ data-model/src/encoding/compact_width.rs
@@ -1,4 +1,5 @@
 use crate::encoding::error::{DecodeError, EncodingConsumerError};
+use crate::encoding::parameters::Encoder;
 use ufotofu::local_nb::{BulkConsumer, BulkProducer};
 
 /// A minimum width of bytes needed to represent a unsigned integer.
@@ -15,35 +16,82 @@ pub enum CompactWidth {
 }
 
 #[derive(Debug)]
-pub struct NotACompactWidthError(pub u8);
+pub struct NotACompactWidthError();
 
 impl CompactWidth {
-    /// Return a new [`CompactWidth`]
+    /// Return a new [`CompactWidth`].
     pub fn new(n: u8) -> Result<CompactWidth, NotACompactWidthError> {
         match n {
             1 => Ok(CompactWidth::One),
             2 => Ok(CompactWidth::Two),
             4 => Ok(CompactWidth::Four),
             8 => Ok(CompactWidth::Eight),
-            _ => Err(NotACompactWidthError(n)),
+            _ => Err(NotACompactWidthError()),
+        }
+    }
+
+    /// Return the width in bytes of this [`CompactSize`].
+    pub fn width(&self) -> usize {
+        match self {
+            CompactWidth::One => 1,
+            CompactWidth::Two => 2,
+            CompactWidth::Four => 4,
+            CompactWidth::Eight => 8,
         }
     }
 }
 
-/// Return the most compact width in bytes (1, 2, 4, or 8) needed to represent a given `u64` as a corresponding 8-bit, 16-bit, 32-bit, or 64-bit number.
-///
-/// [Definition](https://willowprotocol.org/specs/encodings/index.html#compact_width).
-// Doc tests?!
-pub fn compact_width(value: u64) -> CompactWidth {
-    // There is a branchless implementation of this via logarithms, but that's for later and requires benchmarking.
-    if value <= u8::MAX as u64 {
+impl From<u64> for CompactWidth {
+    /// Return the most compact width in bytes (1, 2, 4, or 8) needed to represent a given `u64` as a corresponding 8-bit, 16-bit, 32-bit, or 64-bit number.
+    ///
+    /// [Definition](https://willowprotocol.org/specs/encodings/index.html#compact_width).
+    fn from(value: u64) -> Self {
+        if value <= u8::MAX as u64 {
+            CompactWidth::One
+        } else if value <= u16::MAX as u64 {
+            CompactWidth::Two
+        } else if value <= u32::MAX as u64 {
+            CompactWidth::Four
+        } else {
+            CompactWidth::Eight
+        }
+    }
+}
+
+impl From<u32> for CompactWidth {
+    /// Return the most compact width in bytes (1, 2, 4) needed to represent a given `u32` as a corresponding 8-bit, 16-bit, or 32-bit number.
+    ///
+    /// [Definition](https://willowprotocol.org/specs/encodings/index.html#compact_width).
+    fn from(value: u32) -> Self {
+        if value <= u8::MAX as u32 {
+            CompactWidth::One
+        } else if value <= u16::MAX as u32 {
+            CompactWidth::Two
+        } else {
+            CompactWidth::Four
+        }
+    }
+}
+
+impl From<u16> for CompactWidth {
+    /// Return the most compact width in bytes (1 or 2) needed to represent a given `u16` as a corresponding 8-bit or 16-bit number.
+    ///
+    /// [Definition](https://willowprotocol.org/specs/encodings/index.html#compact_width).
+    fn from(value: u16) -> Self {
+        if value <= u8::MAX as u16 {
+            CompactWidth::One
+        } else {
+            CompactWidth::Two
+        }
+    }
+}
+
+impl From<u8> for CompactWidth {
+    /// Return [`CompactWidth::One`], the only [`CompactWidth`] needed to represent a given `u8`.
+    ///
+    /// [Definition](https://willowprotocol.org/specs/encodings/index.html#compact_width).
+    fn from(_: u8) -> Self {
         CompactWidth::One
-    } else if value <= u16::MAX as u64 {
-        CompactWidth::Two
-    } else if value <= u32::MAX as u64 {
-        CompactWidth::Four
-    } else {
-        CompactWidth::Eight
     }
 }
 
@@ -52,12 +100,7 @@ pub async fn encode_compact_width_be<Consumer: BulkConsumer<Item = u8>>(
     value: u64,
     consumer: &mut Consumer,
 ) -> Result<(), EncodingConsumerError<Consumer::Error>> {
-    let width = match compact_width(value) {
-        CompactWidth::One => 1,
-        CompactWidth::Two => 2,
-        CompactWidth::Four => 4,
-        CompactWidth::Eight => 8,
-    };
+    let width = CompactWidth::from(value).width();
 
     consumer
         .bulk_consume_full_slice(&value.to_be_bytes()[8 - width..])
@@ -120,17 +163,39 @@ mod tests {
 
     #[test]
     fn compact_width_works() {
-        assert_eq!(compact_width(0), CompactWidth::One);
-        assert_eq!(compact_width(u8::MAX as u64), CompactWidth::One);
+        // u64
+        assert_eq!(CompactWidth::from(0_u64), CompactWidth::One);
+        assert_eq!(CompactWidth::from(u8::MAX as u64), CompactWidth::One);
 
-        assert_eq!(compact_width(u8::MAX as u64 + 1), CompactWidth::Two);
-        assert_eq!(compact_width(u16::MAX as u64), CompactWidth::Two);
+        assert_eq!(CompactWidth::from(u8::MAX as u64 + 1), CompactWidth::Two);
+        assert_eq!(CompactWidth::from(u16::MAX as u64), CompactWidth::Two);
 
-        assert_eq!(compact_width(u16::MAX as u64 + 1), CompactWidth::Four);
-        assert_eq!(compact_width(u32::MAX as u64), CompactWidth::Four);
+        assert_eq!(CompactWidth::from(u16::MAX as u64 + 1), CompactWidth::Four);
+        assert_eq!(CompactWidth::from(u32::MAX as u64), CompactWidth::Four);
 
-        assert_eq!(compact_width(u32::MAX as u64 + 1), CompactWidth::Eight);
-        assert_eq!(compact_width(u64::MAX), CompactWidth::Eight);
+        assert_eq!(CompactWidth::from(u32::MAX as u64 + 1), CompactWidth::Eight);
+        assert_eq!(CompactWidth::from(u64::MAX), CompactWidth::Eight);
+
+        // u32
+        assert_eq!(CompactWidth::from(0_u32), CompactWidth::One);
+        assert_eq!(CompactWidth::from(u8::MAX as u32), CompactWidth::One);
+
+        assert_eq!(CompactWidth::from(u8::MAX as u32 + 1), CompactWidth::Two);
+        assert_eq!(CompactWidth::from(u16::MAX as u32), CompactWidth::Two);
+
+        assert_eq!(CompactWidth::from(u16::MAX as u32 + 1), CompactWidth::Four);
+        assert_eq!(CompactWidth::from(u32::MAX), CompactWidth::Four);
+
+        // u16
+        assert_eq!(CompactWidth::from(0_u16), CompactWidth::One);
+        assert_eq!(CompactWidth::from(u8::MAX as u16), CompactWidth::One);
+
+        assert_eq!(CompactWidth::from(u8::MAX as u16 + 1), CompactWidth::Two);
+        assert_eq!(CompactWidth::from(u16::MAX), CompactWidth::Two);
+
+        // u8
+        assert_eq!(CompactWidth::from(0_u8), CompactWidth::One);
+        assert_eq!(CompactWidth::from(u8::MAX), CompactWidth::One);
     }
 
     #[test]
