8ad44bbcb8fc4eb59fd964ff248ec06b7bad2764
Aljoscha Meyer <mail@aljoscha-meyer.de>
2024-07-30 14:56:17 +0200

    Demonstrate usage of syncify

--- data-model/src/encoding/mod.rs
+++ data-model/src/encoding/mod.rs
@@ -1,8 +1,16 @@
 pub mod bytes;
 pub mod compact_width;
 pub mod error;
-pub mod max_power;
 pub mod parameters;
+pub mod parameters_sync;
 pub mod relativity;
 pub(crate) mod shared_buffers;
 pub mod unsigned_int;
+
+pub mod max_power;
+pub mod max_power_sync {
+    use super::max_power;
+
+    pub use max_power::encoding_sync::*;
+    pub use max_power::max_power;
+}
---
+++ data-model/src/encoding/parameters_sync.rs
@@ -0,0 +1,34 @@
+use std::future::Future;
+
+use crate::encoding::error::DecodeError;
+use ufotofu::sync::{BulkConsumer, BulkProducer};
+
+/// A type that can be encoded to a bytestring, ensuring that any value of `Self` maps to exactly one bytestring.
+///
+/// [Definition](https://willowprotocol.org/specs/encodings/index.html#encodings_what)
+pub trait Encodable {
+    /// A function from the set `Self` to the set of bytestrings.
+    ///
+    /// [Definition](https://willowprotocol.org/specs/encodings/index.html#encode_s)
+    fn encode<Consumer>(
+        &self,
+        consumer: &mut Consumer,
+    ) -> Result<(), Consumer::Error>
+    where
+        Consumer: BulkConsumer<Item = u8>;
+}
+
+/// A type that can be decoded from a bytestring, ensuring that every valid encoding maps to exactly one member of `Self`.
+///
+/// [Definition](https://willowprotocol.org/specs/encodings/index.html#encodings_what)
+pub trait Decodable {
+    /// A function from the set of bytestrings to the set of `T`.
+    ///
+    /// [Definition](https://willowprotocol.org/specs/encodings/index.html#decode_s)
+    fn decode<Producer>(
+        producer: &mut Producer,
+    ) -> Result<Self, DecodeError<Producer::Error>>
+    where
+        Producer: BulkProducer<Item = u8>,
+        Self: Sized;
+}
