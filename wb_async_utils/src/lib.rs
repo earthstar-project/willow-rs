2ee7168b29f3eeb91ae87624d7a641414d9d0704
Sam Gwilym <sam@gwil.garden>
2024-11-26 16:16:34 +0100

    Add a mutex, original work do not steal

--- wb_async_utils/src/lib.rs
+++ wb_async_utils/src/lib.rs
@@ -1,2 +1,5 @@
 mod rw;
 pub use rw::*;
+
+mod mutex;
+pub use mutex::*;
--- wb_async_utils/src/mutex.rs
+++ wb_async_utils/src/mutex.rs
@@ -1,7 +1,6 @@
 use core::{
     cell::{Cell, UnsafeCell},
     future::Future,
-    hint::unreachable_unchecked,
     ops::{Deref, DerefMut},
     pin::Pin,
     task::{Context, Poll, Waker},
@@ -9,61 +8,56 @@ use core::{
 use std::collections::VecDeque;
 use std::fmt;
 
-/// An awaitable, single-threaded read-write lock. Allows an arbitrary number of concurrent immutable accesses, or a single mutable access.
+/// An awaitable, single-threaded mutex. Only a single reference to the contents of the mutex can exist at any time.
 ///
-/// Write accesses are parked and waked in FIFO order, but read accesses always have priority.
-/// That is, after a write access has completed, all read accesses are granted.
-/// Only if there are no pending read accesses is the next (in FIFO order) write access granted.
-pub struct RwLock<T> {
+/// All accesses are parked and waked in FIFO order.
+pub struct Mutex<T> {
     value: UnsafeCell<T>,
-    readers: Cell<Option<usize>>, // `None` while writing, `Some(0)` if there are neither readers nor writers
-    parked_reads: UnsafeCell<VecDeque<Waker>>, // push_back to enqueue, pop_front to dequeue
-    parked_writes: UnsafeCell<VecDeque<Waker>>, // push_back to enqueue, pop_front to dequeue
+    currently_used: Cell<bool>,
+    parked: UnsafeCell<VecDeque<Waker>>, // push_back to enqueue, pop_front to dequeue
 }
 
-impl<T> RwLock<T> {
-    /// Creates a new RwLock storing the given value.
+impl<T> Mutex<T> {
+    /// Creates a new mutex storing the given value.
     pub fn new(value: T) -> Self {
-        RwLock {
+        Mutex {
             value: UnsafeCell::new(value),
-            readers: Cell::new(Some(0)),
-            parked_reads: UnsafeCell::new(VecDeque::new()),
-            parked_writes: UnsafeCell::new(VecDeque::new()),
+            currently_used: Cell::new(false),
+            parked: UnsafeCell::new(VecDeque::new()),
         }
     }
 
-    /// Consumes the RwLock and returns the wrapped value.
+    /// Consumes the mutex and returns the wrapped value.
     pub fn into_inner(self) -> T {
         self.value.into_inner()
     }
 
     /// Gives read access to the wrapped value, waiting if necessary.
-    pub async fn read<'lock>(&'lock self) -> ReadGuard<'lock, T> {
+    pub async fn read(&self) -> ReadGuard<T> {
         ReadFuture(self).await
     }
 
     /// Gives read access if doing so is possible without waiting, returns `None` otherwise.
-    pub fn try_read(&self) -> Option<ReadGuard<'_, T>> {
-        let reader_count = self.readers.get()?;
-        self.readers.set(Some(reader_count + 1));
+    pub fn try_read(&self) -> Option<ReadGuard<T>> {
+        if self.currently_used.get() {
+            return None;
+        }
 
-        Some(ReadGuard { lock: self })
+        Some(ReadGuard { mutex: self })
     }
 
     /// Gives write access to the wrapped value, waiting if necessary.
-    pub async fn write<'lock>(&'lock self) -> WriteGuard<'lock, T> {
+    pub async fn write(&self) -> WriteGuard<T> {
         WriteFuture(self).await
     }
 
     /// Gives write access if doing so is possible without waiting, returns `None` otherwise.
-    pub fn try_write(&self) -> Option<WriteGuard<'_, T>> {
-        match self.readers.get() {
-            Some(0) => {
-                self.readers.set(None);
-                Some(WriteGuard { lock: self })
-            }
-            _ => None,
+    pub fn try_write(&self) -> Option<WriteGuard<T>> {
+        if self.currently_used.get() {
+            return None;
         }
+
+        Some(WriteGuard { mutex: self })
     }
 
     /// Sets the wrapped value.
@@ -149,41 +143,25 @@ impl<T> RwLock<T> {
     }
 
     fn wake_next(&self) {
-        debug_assert_eq!(self.readers.get(), Some(0));
-
-        let there_are_no_pending_reads = { (unsafe { &*self.parked_reads.get() }).is_empty() };
-
-        if there_are_no_pending_reads {
-            if let Some(next_write) = (unsafe { &mut *self.parked_writes.get() }).pop_front() {
-                next_write.wake();
-            }
-        } else {
-            for parked_read in (unsafe { &mut *self.parked_reads.get() }).drain(..) {
-                parked_read.wake();
-            }
+        if let Some(waker) = (unsafe { &mut *self.parked.get() }).pop_front() {
+            waker.wake()
         }
     }
 
-    fn park_read(&self, cx: &mut Context<'_>) {
-        // Safe because self.parked_reads is only accessed during `wake_next` and `park_waker` and access does not outlive those functions.
-        let parked_reads = unsafe { &mut *self.parked_reads.get() };
-        parked_reads.push_back(cx.waker().clone());
-    }
-
-    fn park_write(&self, cx: &mut Context<'_>) {
-        // Safe because self.parked_reads is only accessed during `wake_next` and `park_waker` and access does not outlive those functions.
-        let parked_writes = unsafe { &mut *self.parked_writes.get() };
-        parked_writes.push_back(cx.waker().clone());
+    fn park(&self, cx: &mut Context<'_>) {
+        // Safe because self.parked is only accessed during `wake_next` and `park_waker` and access does not outlive those functions.
+        let parked = unsafe { &mut *self.parked.get() };
+        parked.push_back(cx.waker().clone());
     }
 }
 
-impl<T> AsMut<T> for RwLock<T> {
+impl<T> AsMut<T> for Mutex<T> {
     fn as_mut(&mut self) -> &mut T {
         unsafe { &mut *self.value.get() } // Safe because a `&mut RwLock` can never live at the same time as a `ReadGuard`, `WriteGuard` or another `&mut RwLock`
     }
 }
 
-impl<T: fmt::Debug> fmt::Debug for RwLock<T> {
+impl<T: fmt::Debug> fmt::Debug for Mutex<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut d = f.debug_tuple("RwLock");
         match self.try_read() {
@@ -201,87 +179,79 @@ impl<T: fmt::Debug> fmt::Debug for RwLock<T> {
 /// Read-only access to the value stored in a [`RwLock`].
 ///
 /// The wrapped value is accessible via the implementation of `Deref`.
-pub struct ReadGuard<'lock, T> {
-    lock: &'lock RwLock<T>,
+pub struct ReadGuard<'mutex, T> {
+    mutex: &'mutex Mutex<T>,
 }
 
-impl<'lock, T> Drop for ReadGuard<'lock, T> {
+impl<'mutex, T> Drop for ReadGuard<'mutex, T> {
     fn drop(&mut self) {
-        match self.lock.readers.get() {
-            None => unsafe { unreachable_unchecked() }, // ReadGuards are only created when there are no writers
-            Some(reader_count) => {
-                self.lock.readers.set(Some(reader_count - 1)); // never underflows, there is at least one reader (us)
-                if reader_count == 1 {
-                    self.lock.wake_next();
-                }
-            }
-        }
+        self.mutex.currently_used.set(false);
+        self.mutex.wake_next();
     }
 }
 
-impl<'lock, T> Deref for ReadGuard<'lock, T> {
+impl<'mutex, T> Deref for ReadGuard<'mutex, T> {
     type Target = T;
 
     fn deref(&self) -> &T {
-        unsafe { &*self.lock.value.get() } // Safe because a ReadGuard can never live at the same time as a WriteGuard or a `&mut RwLock`
+        unsafe { &*self.mutex.value.get() } // Safe because a ReadGuard can never live at the same time as a WriteGuard or a `&mut RwLock`
     }
 }
 
 /// Read and write access access to the value stored in a [`RwLock`].
 ///
 /// The wrapped value is accessible via the implementation of `Deref` and `DerefMut`.
-pub struct WriteGuard<'lock, T> {
-    lock: &'lock RwLock<T>,
+pub struct WriteGuard<'mutex, T> {
+    mutex: &'mutex Mutex<T>,
 }
 
-impl<'lock, T> Drop for WriteGuard<'lock, T> {
+impl<'mutex, T> Drop for WriteGuard<'mutex, T> {
     fn drop(&mut self) {
-        self.lock.readers.set(Some(0)); // Guaranteed to have been `None` before.
-
-        self.lock.wake_next();
+        self.mutex.currently_used.set(false);
+        self.mutex.wake_next();
     }
 }
 
-impl<'lock, T> Deref for WriteGuard<'lock, T> {
+impl<'mutex, T> Deref for WriteGuard<'mutex, T> {
     type Target = T;
 
     fn deref(&self) -> &T {
-        unsafe { &*self.lock.value.get() } // Safe because a `&WriteGuard` can never live at the same time as a `&mut WriteGuard` or a `&mut RwLock`
+        unsafe { &*self.mutex.value.get() } // Safe because a `&WriteGuard` can never live at the same time as a `&mut WriteGuard` or a `&mut RwLock`
     }
 }
 
-impl<'lock, T> DerefMut for WriteGuard<'lock, T> {
+impl<'mutex, T> DerefMut for WriteGuard<'mutex, T> {
     fn deref_mut(&mut self) -> &mut T {
-        unsafe { &mut *self.lock.value.get() } // Safe because a `&mut WriteGuard` can never live at the same time as another `&mut WriteGuard` or a `&mut RwLock`
+        unsafe { &mut *self.mutex.value.get() } // Safe because a `&mut WriteGuard` can never live at the same time as another `&mut WriteGuard` or a `&mut RwLock`
     }
 }
 
-struct ReadFuture<'lock, T>(&'lock RwLock<T>);
+struct ReadFuture<'mutex, T>(&'mutex Mutex<T>);
 
-impl<'lock, T> Future for ReadFuture<'lock, T> {
-    type Output = ReadGuard<'lock, T>;
+impl<'mutex, T> Future for ReadFuture<'mutex, T> {
+    type Output = ReadGuard<'mutex, T>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         match self.0.try_read() {
             Some(guard) => Poll::Ready(guard),
             None => {
-                self.0.park_read(cx);
+                self.0.park(cx);
                 Poll::Pending
             }
         }
     }
 }
 
-struct WriteFuture<'lock, T>(&'lock RwLock<T>);
+struct WriteFuture<'mutex, T>(&'mutex Mutex<T>);
 
-impl<'lock, T> Future for WriteFuture<'lock, T> {
-    type Output = WriteGuard<'lock, T>;
+impl<'mutex, T> Future for WriteFuture<'mutex, T> {
+    type Output = WriteGuard<'mutex, T>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         match self.0.try_write() {
             Some(guard) => Poll::Ready(guard),
             None => {
-                self.0.park_write(cx);
+                self.0.park(cx);
                 Poll::Pending
             }
         }
