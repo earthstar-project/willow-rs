57b8face8bce2b0bf1d6e4b218655c089be9a116
Aljoscha Meyer <mail@aljoscha-meyer.de>
2024-12-01 10:47:28 +0100

    Add relativity to ufotofu_codec

--- ufotofu_codec/src/lib.rs
+++ ufotofu_codec/src/lib.rs
@@ -16,11 +16,25 @@
 //! - there are no two distinct `t1, t2` in `T` such that there is a single encoding `enc` such that `(t1, enc)` and `(t2, enc)` are both in the relation (i.e., the relation is [injective](https://en.wikipedia.org/wiki/Injective_function)), and
 //! - for any two bytestrings `enc1, enc2` in the relation, neither is a strict prefix of the other (i.e., we have a [prefix code](https://en.wikipedia.org/wiki/Prefix_code)).
 //!
+//! ## Traits
+//!
 //! The [`Encodable`] and [`Decodable`] trait describe how to convert between the domain of encodable values and the domain of finite bytestrings. Further traits specialise these:
 //!
 //! - [`EncodableCanonic`] and [`DecodableCanonic`] allow working with canonic subsets of an encoding relation, i.e., encodings where there is a one-to-one correspondence between values and codes.
 //! - [`EncodableSync`] and [`DecodableSync`] are marker traits that signal that all asynchrony in encoding/decoding stems from the consumer/producer, but all further computations are synchronous. When using a consumer/producer that never blocks either, this allows for synchronous encoding/decoding.
 //! - [`EncodableKnownSize`] describes how to compute the size of an encoding without actually performing the encoding. Useful when the size needs to be known in advance (length-prefixing, or encoding into a slice).
+//!
+//! ## Relativity
+//!
+//! All traits support encoding and decoding *relative* to some reference value that is supplied dynamically at runtime. For absolute encodings, set the type of the reference value to `()`; this enables convenience methods that do not require passing a `&()` all the time.
+//!
+//! Formally, each possible reference value defines an encoding relation that is independent from the relations defined by any other reference values. All API contracts that are stated in the documentation must hold for the same reference value. As an example, consider an encoding for integers that encodes relative to a reference value by encoding the difference between the value to encode and the reference value. Then, when encoding any two distinct values relative to the reference value `5`, the resulting encodings must be different. But when encoding some value `x` relative to `5` and another value `y` relative to `17`, then it is perfectly fine for both encodings to be the same bytestring.
+//!
+//! ## Encoders and Decoders
+//!
+//! TODO
+//!
+//! ## Property Testing
 //!
 //! When the `dev` feature is enabled, the [`proptest`] module provides helpers for writing property tests for checking the invariants that implementations of the traits of this crate must uphold.
 
@@ -117,17 +131,47 @@ impl<E: Display> Display for DecodeError<E> {
 /// - The result of decoding must depend only on the decoded bytes, not on details of the producer such as when it yields or how many items it exposes at a time.
 /// - For types that also implement `Encodable` and `Eq`, encoding a value and then decoding it must yield a value equal to the original.
 pub trait Decodable: Sized {
+    /// The type for reporting that the bytestring to decode was not a valid encoding of any value of type `Self`.
+    ///
+    /// The [`DecodeError`] type is a recommended choice for this type parameter when it is not important to trace in detail why exactly decoding went wrong.
     type Error;
+
+    /// The type of values relative to which we decode. Set to `()` for absolute encodings.
+    type DecodeRelativeTo;
+
+    /// Decodes the bytes produced by the given producer into a `Self`, or yields an error if the producer does not produce a valid encoding.
+    fn relative_decode<P>(
+        producer: &mut P,
+        r: &Self::DecodeRelativeTo,
+    ) -> impl Future<Output = Result<Self, Self::Error>>
+    where
+        P: BulkProducer<Item = u8>,
+        Self: Sized;
 
     /// Decodes the bytes produced by the given producer into a `Self`, or yields an error if the producer does not produce a valid encoding.
     fn decode<P>(producer: &mut P) -> impl Future<Output = Result<Self, Self::Error>>
     where
         P: BulkProducer<Item = u8>,
-        Self: Sized;
+        Self: Sized,
+        Self: Decodable<DecodeRelativeTo = ()>,
+    {
+        Self::relative_decode(producer, &())
+    }
 
     /// Decodes from a slice instead of a producer.
-    fn decode_from_slice(enc: &[u8]) -> impl Future<Output = Result<Self, Self::Error>> {
-        async { Self::decode(&mut FromSlice::new(enc)).await }
+    fn relative_decode_from_slice(
+        enc: &[u8],
+        r: &Self::DecodeRelativeTo,
+    ) -> impl Future<Output = Result<Self, Self::Error>> {
+        async { Self::relative_decode(&mut FromSlice::new(enc), r).await }
+    }
+
+    /// Decodes from a slice instead of a producer.
+    fn decode_from_slice(enc: &[u8]) -> impl Future<Output = Result<Self, Self::Error>>
+    where
+        Self: Decodable<DecodeRelativeTo = ()>,
+    {
+        Self::relative_decode_from_slice(enc, &())
     }
 }
 
@@ -140,22 +184,50 @@ pub trait Decodable: Sized {
 /// - No encoding must be a prefix of a different encoding.
 /// - For types that also implement `Decodable` and `Eq`, encoding a value and then decoding it must yield a value equal to the original.
 pub trait Encodable {
+    /// The type of values relative to which we encode. Set to `()` for absolute encodings.
+    type EncodeRelativeTo;
+
     /// Writes an encoding of `&self` into the given consumer.
-    fn encode<C>(&self, consumer: &mut C) -> impl Future<Output = Result<(), C::Error>>
+    fn relative_encode<C>(
+        &self,
+        consumer: &mut C,
+        r: &Self::EncodeRelativeTo,
+    ) -> impl Future<Output = Result<(), C::Error>>
     where
         C: BulkConsumer<Item = u8>;
 
+    /// Writes an absolute encoding of `&self` into the given consumer.
+    fn encode<C>(&self, consumer: &mut C) -> impl Future<Output = Result<(), C::Error>>
+    where
+        C: BulkConsumer<Item = u8>,
+        Self: Encodable<EncodeRelativeTo = ()>,
+    {
+        self.relative_encode(consumer, &())
+    }
+
     #[cfg(feature = "alloc")]
     /// Encodes into a Vec instead of a given consumer.
-    fn encode_into_vec(&self) -> impl Future<Output = Vec<u8>> {
+    fn relative_encode_into_vec(
+        &self,
+        r: &Self::EncodeRelativeTo,
+    ) -> impl Future<Output = Vec<u8>> {
         async {
             let mut c = IntoVec::new();
 
-            match self.encode(&mut c).await {
+            match self.relative_encode(&mut c, r).await {
                 Ok(()) => c.into_vec(),
                 Err(_) => unreachable!(),
             }
         }
+    }
+
+    #[cfg(feature = "alloc")]
+    /// Absolutely encodes into a Vec instead of a given consumer.
+    fn encode_into_vec(&self) -> impl Future<Output = Vec<u8>>
+    where
+        Self: Encodable<EncodeRelativeTo = ()>,
+    {
+        self.relative_encode_into_vec(&())
     }
 }
 
@@ -163,20 +235,40 @@ pub trait Encodable {
 ///
 /// API contract: `self.encode(c)` must write exactly `self.len_of_encoding()` many bytes into `c`.
 pub trait EncodableKnownSize: Encodable {
-    /// Computes the size of the encoding in bytes. Calling [`encode`](Encodable::encode) must feed exactly that many bytes into the consumer.
-    fn len_of_encoding(&self) -> usize;
+    /// Computes the size of the encoding in bytes. Calling [`relative_encode`](Encodable::relative_encode) must feed exactly that many bytes into the consumer.
+    fn relative_len_of_encoding(&self, r: &Self::EncodeRelativeTo) -> usize;
+
+    /// Computes the size of the absolute encoding in bytes. Calling [`encode`](Encodable::encode) must feed exactly that many bytes into the consumer.
+    fn len_of_encoding(&self) -> usize
+    where
+        Self: Encodable<EncodeRelativeTo = ()>,
+    {
+        self.relative_len_of_encoding(&())
+    }
 
     #[cfg(feature = "alloc")]
     /// Encodes into a boxed slice instead of a given consumer.
-    fn encode_into_boxed_slice(&self) -> impl Future<Output = Box<[u8]>> {
+    fn relative_encode_into_boxed_slice(
+        &self,
+        r: &Self::EncodeRelativeTo,
+    ) -> impl Future<Output = Box<[u8]>> {
         async {
-            let mut c = IntoVec::with_capacity(self.len_of_encoding());
+            let mut c = IntoVec::with_capacity(self.relative_len_of_encoding(r));
 
-            match self.encode(&mut c).await {
+            match self.relative_encode(&mut c, r).await {
                 Ok(()) => c.into_vec().into_boxed_slice(),
                 Err(_) => unreachable!(),
             }
         }
+    }
+
+    #[cfg(feature = "alloc")]
+    /// Absolutely encodes into a boxed slice instead of a given consumer.
+    fn encode_into_boxed_slice(&self) -> impl Future<Output = Box<[u8]>>
+    where
+        Self: Encodable<EncodeRelativeTo = ()>,
+    {
+        self.relative_encode_into_boxed_slice(&())
     }
 }
 
@@ -188,33 +280,81 @@ pub trait EncodableKnownSize: Encodable {
 ///
 /// There is no corresponding `EncodableCanonic` trait, because `Encodable` already fulfils the dual requirement of two nonequal values yielding nonequal codes.
 pub trait DecodableCanonic: Decodable {
+    /// The type for reporting that the bytestring to decode was not a valid canonic encoding of any value of type `Self`.
+    ///
+    /// Typically contains at least as much information as [`Self::Error`](Decodable::Error). If the encoding relation implemented by [`Decodable`] is already canonic, then [`ErrorCanonic`](DecodableCanonic::ErrorCanonic) should be equal to [`Self::Error`](Decodable::Error).
     type ErrorCanonic: From<Self::Error>;
 
     /// Decodes the bytes produced by the given producer into a `Self`, and errors if the input encoding is not the canonical one.
-    fn decode_canonic<P>(producer: &mut P) -> impl Future<Output = Result<Self, Self::Error>>
+    fn relative_decode_canonic<P>(
+        producer: &mut P,
+        r: &Self::DecodeRelativeTo,
+    ) -> impl Future<Output = Result<Self, Self::Error>>
     where
         P: BulkProducer<Item = u8>,
         Self: Sized;
 
+    ///Absolutely decodes the bytes produced by the given producer into a `Self`, and errors if the input encoding is not the canonical one.
+    fn decode_canonic<P>(producer: &mut P) -> impl Future<Output = Result<Self, Self::Error>>
+    where
+        P: BulkProducer<Item = u8>,
+        Self: Sized,
+        Self: Decodable<DecodeRelativeTo = ()>,
+    {
+        Self::relative_decode_canonic(producer, &())
+    }
+
     /// Decodes from a slice instead of a producer, and errors if the input encoding is not the canonical one.
-    ///
-    /// This method must not [`close`](ufotofu::Consumer::close) the consumer.
-    fn decode_canonic_from_slice(enc: &[u8]) -> impl Future<Output = Result<Self, Self::Error>> {
-        async { Self::decode_canonic(&mut FromSlice::new(enc)).await }
+    fn relative_decode_canonic_from_slice(
+        enc: &[u8],
+        r: &Self::DecodeRelativeTo,
+    ) -> impl Future<Output = Result<Self, Self::Error>> {
+        async { Self::relative_decode_canonic(&mut FromSlice::new(enc), r).await }
+    }
+
+    /// Absolutely decodes from a slice instead of a producer, and errors if the input encoding is not the canonical one.
+    fn decode_canonic_from_slice(enc: &[u8]) -> impl Future<Output = Result<Self, Self::Error>>
+    where
+        Self: Decodable<DecodeRelativeTo = ()>,
+    {
+        Self::relative_decode_canonic_from_slice(enc, &())
     }
 }
 
 /// A decodable that introduces no asynchrony beyond that of `.await`ing the producer. This is essentially a marker trait by which to tell other programmers about this property. As a practical benefit, the default methods of this trait allow for convenient synchronous decoding via producers that are known to never block.
 pub trait DecodableSync: Decodable {
     /// Synchronously decodes from a slice instead of a producer.
-    fn sync_decode_from_slice(enc: &[u8]) -> Result<Self, Self::Error> {
+    fn sync_relative_decode_from_slice(
+        enc: &[u8],
+        r: &Self::DecodeRelativeTo,
+    ) -> Result<Self, Self::Error> {
+        pollster::block_on(Self::relative_decode_from_slice(enc, r))
+    }
+
+    /// Synchronously and absolutely decodes from a slice instead of a producer.
+    fn sync_decode_from_slice(enc: &[u8]) -> Result<Self, Self::Error>
+    where
+        Self: Decodable<DecodeRelativeTo = ()>,
+    {
         pollster::block_on(Self::decode_from_slice(enc))
     }
 
-    /// Asynchronously decodes from a slice instead of a producer, and errors if the input encoding is not the canonical one.
+    /// Synchronously decodes from a slice instead of a producer, and errors if the input encoding is not the canonical one.
+    fn sync_relative_decode_canonic_from_slice(
+        enc: &[u8],
+        r: &Self::DecodeRelativeTo,
+    ) -> Result<Self, Self::Error>
+    where
+        Self: DecodableCanonic,
+    {
+        pollster::block_on(Self::relative_decode_canonic_from_slice(enc, r))
+    }
+
+    /// Synchronously and absolutely decodes from a slice instead of a producer, and errors if the input encoding is not the canonical one.
     fn sync_decode_canonic_from_slice(enc: &[u8]) -> Result<Self, Self::Error>
     where
         Self: DecodableCanonic,
+        Self: Decodable<DecodeRelativeTo = ()>,
     {
         pollster::block_on(Self::decode_canonic_from_slice(enc))
     }
@@ -226,24 +366,36 @@ pub trait DecodableSync: Decodable {
 pub trait EncodableSync: Encodable {
     #[cfg(feature = "alloc")]
     /// Synchronously encodes into a Vec instead of a given consumer.
-    ///
-    /// This method must not [`close`](ufotofu::Consumer::close) the consumer.
-    fn sync_encode_into_vec(&self) -> Vec<u8> {
+    fn sync_relative_encode_into_vec(&self, r: &Self::EncodeRelativeTo) -> Vec<u8> {
+        pollster::block_on(self.relative_encode_into_vec(r))
+    }
+
+    /// Synchronously and absolutely encodes into a Vec instead of a given consumer.
+    fn sync_encode_into_vec(&self) -> Vec<u8>
+    where
+        Self: Encodable<EncodeRelativeTo = ()>,
+    {
         pollster::block_on(self.encode_into_vec())
     }
 
     #[cfg(feature = "alloc")]
     /// Synchronously encodes into a boxed slice instead of a given consumer.
-    ///
-    /// This method must not [`close`](ufotofu::Consumer::close) the consumer.
+    fn sync_relative_encode_into_boxed_slice(&self, r: &Self::EncodeRelativeTo) -> Box<[u8]>
+    where
+        Self: EncodableKnownSize,
+    {
+        pollster::block_on(self.relative_encode_into_boxed_slice(r))
+    }
+
+    #[cfg(feature = "alloc")]
+    /// Synchronously and absolutely encodes into a boxed slice instead of a given consumer.
     fn sync_encode_into_boxed_slice(&self) -> Box<[u8]>
     where
         Self: EncodableKnownSize,
+        Self: Encodable<EncodeRelativeTo = ()>,
     {
         pollster::block_on(self.encode_into_boxed_slice())
     }
 }
 
-// TODO Relativity
-
-// TODO Encoder, EncoderCanonic, Decoder, DecoderCanonic, relative variants?
+// TODO Encoder, Decoder, DecoderCanonic; absolute and relative; (owned and borrowed for the relative ones?)
--- ufotofu_codec/src/proptest.rs
+++ ufotofu_codec/src/proptest.rs
@@ -1,83 +1,83 @@
-use core::convert::Infallible;
-use core::fmt::Debug;
-use std::format;
+// use core::convert::Infallible;
+// use core::fmt::Debug;
+// use std::format;
 
-use ufotofu::consumer::TestConsumer;
+// use ufotofu::consumer::TestConsumer;
 
-use crate::Encodable;
+// use crate::Encodable;
 
-/// Irrespective of the blocking/yielding and sizes of exposed slots of a consumer, encoding always produces the same encoding.
-async fn assert_encoding_does_not_care_about_consumer_details<T>(
-    t: &T,
-    mut c1: TestConsumer<u8, Infallible, ()>,
-    mut c2: TestConsumer<u8, Infallible, ()>,
-) where
-    T: Debug + Eq + Encodable,
-{
-    let res1 = t.encode(&mut c1).await;
-    let res2 = t.encode(&mut c2).await;
-
-    let consumed1 = c1.consumed();
-    let consumed2 = c2.consumed();
-    let common_len = core::cmp::min(consumed1.len(), consumed2.len());
-
-    let status = match (res1, res2) {
-        (Ok(()), Ok(())) => format!("Neither consumer errored."),
-        (Err(()), Ok(())) => format!(
-            "First consumer errored after {} bytes, second did not error.",
-            consumed1.len()
-        ),
-        (Ok(()), Err(())) => format!(
-            "First consumer did not error, second consumer errored after {} bytes.",
-            consumed2.len()
-        ),
-        (Err(()), Err(())) => format!(
-            "First consumer errored after {} bytes, second consumer errored after {} bytes.",
-            consumed1.len(),
-            consumed2.len()
-        ),
-    };
-
-    assert_eq!(
-        &consumed1[..common_len],
-        &consumed2[..common_len],
-        "The same value produced two different (prefixes of) encodings for two different consumers.\n\nValue: {:?}\n\n{status}\n\n\nFirst Consumer: {:?}\n\nFirst Encoding: {:?}\n\n\nSecond Consumer: {:?}\n\nSecond Encoding: {:?}", t, c1, consumed1, c2, consumed2, 
-    );
-}
-
-// async fn assert_distinct_values_produce_distinct_encodings<T>(
-//     t1: &T,
-//     t2: &T,
-//     c: TestConsumer<u8, Infallible, ()>,
+// /// Irrespective of the blocking/yielding and sizes of exposed slots of a consumer, encoding always produces the same encoding.
+// async fn assert_encoding_does_not_care_about_consumer_details<T>(
+//     t: &T,
+//     mut c1: TestConsumer<u8, Infallible, ()>,
+//     mut c2: TestConsumer<u8, Infallible, ()>,
 // ) where
-//     T: Eq + Encodable,
+//     T: Debug + Eq + Encodable,
 // {
-//     if t1 != t2 {
-//         let mut c1 = c.clone();
-//         let mut c2 = c;
+//     let res1 = t.relative_encode(&mut c1).await;
+//     let res2 = t.relative_encode(&mut c2).await;
 
-//         if t1.encode(&mut c1).await != t2.encode(&mut c2).await {
-//             false
-//         } else {
-//             c1 == c2
-//         }
-//     }
+//     let consumed1 = c1.consumed();
+//     let consumed2 = c2.consumed();
+//     let common_len = core::cmp::min(consumed1.len(), consumed2.len());
+
+//     let status = match (res1, res2) {
+//         (Ok(()), Ok(())) => format!("Neither consumer errored."),
+//         (Err(()), Ok(())) => format!(
+//             "First consumer errored after {} bytes, second did not error.",
+//             consumed1.len()
+//         ),
+//         (Ok(()), Err(())) => format!(
+//             "First consumer did not error, second consumer errored after {} bytes.",
+//             consumed2.len()
+//         ),
+//         (Err(()), Err(())) => format!(
+//             "First consumer errored after {} bytes, second consumer errored after {} bytes.",
+//             consumed1.len(),
+//             consumed2.len()
+//         ),
+//     };
+
+//     assert_eq!(
+//         &consumed1[..common_len],
+//         &consumed2[..common_len],
+//         "The same value produced two different (prefixes of) encodings for two different consumers.\n\nValue: {:?}\n\n{status}\n\n\nFirst Consumer: {:?}\n\nFirst Encoding: {:?}\n\n\nSecond Consumer: {:?}\n\nSecond Encoding: {:?}", t, c1, consumed1, c2, consumed2, 
+//     );
 // }
 
-// async fn assert_encodings_are_prefix_free<T>(t1: &T, t2: &T, c: TestConsumer<u8, Infallible, ()>)
-// where
-//     T: Eq + Encodable,
-// {
-//     if t1 == t2 {
-//         true
-//     } else {
-//         let mut c1 = c.clone();
-//         let mut c2 = c;
+// // async fn assert_distinct_values_produce_distinct_encodings<T>(
+// //     t1: &T,
+// //     t2: &T,
+// //     c: TestConsumer<u8, Infallible, ()>,
+// // ) where
+// //     T: Eq + Encodable,
+// // {
+// //     if t1 != t2 {
+// //         let mut c1 = c.clone();
+// //         let mut c2 = c;
 
-//         if t1.encode(&mut c1).await != t2.encode(&mut c2).await {
-//             false
-//         } else {
-//             c1 == c2
-//         }
-//     }
-// }
+// //         if t1.encode(&mut c1).await != t2.encode(&mut c2).await {
+// //             false
+// //         } else {
+// //             c1 == c2
+// //         }
+// //     }
+// // }
+
+// // async fn assert_encodings_are_prefix_free<T>(t1: &T, t2: &T, c: TestConsumer<u8, Infallible, ()>)
+// // where
+// //     T: Eq + Encodable,
+// // {
+// //     if t1 == t2 {
+// //         true
+// //     } else {
+// //         let mut c1 = c.clone();
+// //         let mut c2 = c;
+
+// //         if t1.encode(&mut c1).await != t2.encode(&mut c2).await {
+// //             false
+// //         } else {
+// //             c1 == c2
+// //         }
+// //     }
+// // }
