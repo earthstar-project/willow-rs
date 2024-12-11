4c05d884fb5679739c023bf1251831e003767f3b
Sam Gwilym <sam@gwil.garden>
2024-12-11 11:39:49 +0100

    Migrate area <> area encoding fuzz tests

--- fuzz/fuzz_targets/enc_entry_rel_area.rs
+++ fuzz/fuzz_targets/enc_entry_rel_area.rs
@@ -1,7 +1,7 @@
 #![no_main]
 
 use ufotofu_codec::{fuzz_relative_all, Blame};
-use willow_data_model::{grouping::Area, Entry};
-use willow_fuzz::placeholder_params::{FakeNamespaceId, FakePayloadDigest, FakeSubspaceId};
+use willow_data_model::grouping::Area;
+use willow_fuzz::placeholder_params::FakeSubspaceId;
 
-fuzz_relative_all!(Entry<16, 16, 16, FakeNamespaceId, FakeSubspaceId, FakePayloadDigest>; (FakeNamespaceId, Area<16, 16, 16, FakeSubspaceId>); Blame; Blame);
+fuzz_relative_all!(Area<16, 16, 16, FakeSubspaceId>; Area<16, 16, 16, FakeSubspaceId>; Blame; Blame);
