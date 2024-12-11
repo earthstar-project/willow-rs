57c1e0b0b50319e3eea2df71e5d68fcba628af34
Sam Gwilym <sam@gwil.garden>
2024-12-11 11:36:06 +0100

    Migrate entry <> range3d fuzz tests

---
+++ fuzz/fuzz_targets/enc_entry_rel_range3d.rs
@@ -0,0 +1,7 @@
+#![no_main]
+
+use ufotofu_codec::{fuzz_relative_all, Blame};
+use willow_data_model::{grouping::Range3d, Entry};
+use willow_fuzz::placeholder_params::{FakeNamespaceId, FakePayloadDigest, FakeSubspaceId};
+
+fuzz_relative_all!(Entry<16, 16, 16, FakeNamespaceId, FakeSubspaceId, FakePayloadDigest>; (FakeNamespaceId, Range3d<16, 16, 16, FakeSubspaceId>); Blame; Blame);
