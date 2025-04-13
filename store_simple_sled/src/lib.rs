b7c443e317f5c2d38c01a0a3b8eb502ec8c70a01
Sam Gwilym <sam@gwil.garden>
2025-04-13 12:24:20 +0200

    Add working example for using StoreSimpleSled

--- store_simple_sled/src/lib.rs
+++ store_simple_sled/src/lib.rs
@@ -6,6 +6,24 @@
 //! - *Simple*, hence it has a straightforward implementation without the use of fancy data structures.
 //! - Uses [sled](https://docs.rs/sled/latest/sled/) under the hood.
 //!
+//! ```
+//! # use willow_store_simple_sled::StoreSimpleSled;
+//! use willow_25::{ NamespaceId25, SubspaceId25, PayloadDigest25, AuthorisationToken25 };
+//!
+//! let db = sled::open("my_db").unwrap();
+//! let namespace = NamespaceId25::new_communal();
+//!
+//! let store = StoreSimpleSled::<
+//!     1024,
+//!     1024,
+//!     1024,
+//!     NamespaceId25,
+//!     SubspaceId25,
+//!     PayloadDigest25,
+//!     AuthorisationToken25
+//! >::new(&namespace, db).unwrap();
+//! ```
+//!
 //! # Performance considerations
 //!
 //! - Read and write performance should be adequate.
