71bf506ebc3244f2f3017c3859026a8469dc1021
Aljoscha Meyer <mail@aljoscha-meyer.de>
2025-09-17 16:37:02 +0200

    Make ReconciliationSendEntry codec fuzzable

--- fuzz/fuzz_targets/enc_reconciliationsendentry_rel_pair.rs
+++ fuzz/fuzz_targets/enc_reconciliationsendentry_rel_pair.rs
@@ -8,18 +8,130 @@ use willow_fuzz::placeholder_params::{
 
 use ufotofu_codec::{fuzz_relative_known_size, Blame};
 
-fuzz_relative_known_size!(
-    ReconciliationSendEntry<16, 16, 16, FakeNamespaceId, FakeSubspaceId, FakePayloadDigest, FakeAuthorisationToken>;
-    (
-        (&FakeNamespaceId, &Range3d<16, 16, 16, FakeSubspaceId>),
-        &AuthorisedEntry<16, 16, 16, FakeNamespaceId, FakeSubspaceId, FakePayloadDigest, FakeAuthorisationToken>,
-    );
-    Blame;
-    |msg: &ReconciliationSendEntry<16, 16, 16, FakeSubspaceId>, range:  &(
-        (&FakeNamespaceId, &Range3d<16, 16, 16, FakeSubspaceId>),
-        &AuthorisedEntry<16, 16, 16, FakeNamespaceId, FakeSubspaceId, FakePayloadDigest, FakeAuthorisationToken>,
-    )| {
-        true
-    }
+use libfuzzer_sys::fuzz_target;
 
-);
+use core::num::NonZeroUsize;
+
+use ufotofu::consumer::TestConsumer;
+
+use ufotofu_codec::proptest::{
+    assert_relative_basic_invariants, assert_relative_canonic_invariants,
+    assert_relative_known_size_invariants,
+};
+
+fuzz_target!(|data: (
+    (
+        (FakeNamespaceId, Range3d<16, 16, 16, FakeSubspaceId>),
+        AuthorisedEntry<
+            16,
+            16,
+            16,
+            FakeNamespaceId,
+            FakeSubspaceId,
+            FakePayloadDigest,
+            FakeAuthorisationToken,
+        >,
+    ),
+    ReconciliationSendEntry<
+        16,
+        16,
+        16,
+        FakeNamespaceId,
+        FakeSubspaceId,
+        FakePayloadDigest,
+        FakeAuthorisationToken,
+    >,
+    ReconciliationSendEntry<
+        16,
+        16,
+        16,
+        FakeNamespaceId,
+        FakeSubspaceId,
+        FakePayloadDigest,
+        FakeAuthorisationToken,
+    >,
+    TestConsumer<u8, (), ()>,
+    TestConsumer<u8, (), ()>,
+    Box<[u8]>,
+    Box<[NonZeroUsize]>,
+    Box<[NonZeroUsize]>,
+    Box<[bool]>,
+    Box<[bool]>,
+)| {
+    let (
+        r,
+        t1,
+        t2,
+        c1,
+        c2,
+        potential_encoding1,
+        exposed_items_sizes1,
+        exposed_items_sizes2,
+        yield_pattern1,
+        yield_pattern2,
+    ) = data;
+
+    let namespace_id = (r.0).0;
+    let range_3d = (r.0).1;
+    let authed_entry = r.1;
+
+    let actual_r = ((&namespace_id, &range_3d), &authed_entry);
+
+    pollster::block_on(async {
+        assert_relative_basic_invariants::<
+            ReconciliationSendEntry<
+                16,
+                16,
+                16,
+                FakeNamespaceId,
+                FakeSubspaceId,
+                FakePayloadDigest,
+                FakeAuthorisationToken,
+            >,
+            (
+                (&FakeNamespaceId, &Range3d<16, 16, 16, FakeSubspaceId>),
+                &AuthorisedEntry<
+                    16,
+                    16,
+                    16,
+                    FakeNamespaceId,
+                    FakeSubspaceId,
+                    FakePayloadDigest,
+                    FakeAuthorisationToken,
+                >,
+            ),
+            Blame,
+        >(
+            &actual_r,
+            &t1,
+            &t2,
+            c1,
+            c2,
+            &potential_encoding1,
+            exposed_items_sizes1,
+            exposed_items_sizes2,
+            yield_pattern1,
+            yield_pattern2,
+        )
+        .await;
+
+        assert_relative_known_size_invariants(&actual_r, &t1).await;
+    });
+});
+// };
+
+// fuzz_relative_known_size!(
+//     ReconciliationSendEntry<16, 16, 16, FakeNamespaceId, FakeSubspaceId, FakePayloadDigest, FakeAuthorisationToken>;
+//     (
+//         (&FakeNamespaceId, &Range3d<16, 16, 16, FakeSubspaceId>),
+//         &AuthorisedEntry<16, 16, 16, FakeNamespaceId, FakeSubspaceId, FakePayloadDigest, FakeAuthorisationToken>,
+//     );
+//     Blame;
+//     |msg: &ReconciliationSendEntry<16, 16, 16, FakeSubspaceId>, range:  &(
+//         (&FakeNamespaceId, &Range3d<16, 16, 16, FakeSubspaceId>),
+//         &AuthorisedEntry<16, 16, 16, FakeNamespaceId, FakeSubspaceId, FakePayloadDigest, FakeAuthorisationToken>,
+//     )| {
+//         true
+//     }
+
+// );
